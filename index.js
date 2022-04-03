0676f7cb4f5fa31bbf658c546c3d726ec7b78586
Christie Yu <43098796+christieyu@users.noreply.github.com>
2022-04-03 06:36:16 -0400

    added collections/recent template

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// index.js using https://auth0.com/blog/create-a-simple-and-stylish-node-express-app/
+// index.js based off https://auth0.com/blog/create-a-simple-and-stylish-node-express-app/
 
 // Required External Modules
 const express = require("express");
@@ -12,6 +12,9 @@ const port = process.env.PORT || "8000";
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 
 // Routes Definitions
 app.get("/", (req, res) => {
@@ -19,7 +22,22 @@ app.get("/", (req, res) => {
     res.render("index", { 
         title: "Home", 
         date_month: date.toLocaleString('default', { month: 'long' }).toUpperCase(), 
-        date_day: date.getDate() });
+        date_day: date.getDate() 
+    });
+});
+
+app.get("/collections", (req, res) => {
+    date = new Date()
+    res.render("collections", { 
+        title: "Collections",
+    });
+});
+
+app.get("/recent", (req, res) => {
+    date = new Date()
+    res.render("recent", { 
+        title: "Recently Viewed",
+    });
 });
 
 // Server Activation
